fb11b2075a2d6ca2cd1fb0b4b166317b5ddb3397
sirmcclay <mcclay.design@gmail.com>
2019-11-18 06:04:20 -0300

    Star list - making

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View } from 'react-native';
 import api from '../../services/api';
 
-// import { Container } from './styles';
+import { Container, Header, Avatar, Name, Bio } from './styles';
 
 export default class User extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -30,8 +29,27 @@ export default class User extends Component {
   }
 
   render() {
+    const { navigation } = this.props;
     const { stars } = this.state;
 
-    return <View />;
+    const user = navigation.getParam('user');
+
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        <Stars
+          data={stars}
+          keyExtractor={star =>String(star.id)}
+          renderItem={({ item }) => (
+
+          )}
+        />
+      </Container>
+    );
   }
 }
